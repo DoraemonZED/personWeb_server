b8aaed889f8d175afa4845673c6e0ab612c76ea9
poloking <46064541+DoraemonZED@users.noreply.github.com>
2021-11-03 16:58:29 +0800

    Update user.js

--- routers/user.js
+++ routers/user.js
@@ -1,12 +1,12 @@
 const router = require('koa-router')()
-const user = require('../model/user')
+//const user = require('../model/user')
 
 router.get('/login', async ctx => {
   // let a = await user.create({ name:'admin', pwd:'123456' })
-  let a = await user.find({ pwd:'123456' })
+  //let a = await user.find({ pwd:'123456' })
   ctx.body = {
     c:123,
-    a
+    //a
   }
 })
 
