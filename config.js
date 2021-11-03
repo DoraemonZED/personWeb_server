79fd0d3eff800479ddbc7126f0144ff5f9b6a53f
poloking <46064541+DoraemonZED@users.noreply.github.com>
2021-11-03 10:26:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
 
-  prot:5502,//服务器端口
+  prot:9901,//服务器端口
 
   mdbport:'mongodb://127.0.0.1:27017/mydatabase',//mongodb数据库
 
-  Authorization = {
+  Authorization : {
     jwtSecret: 'JWTSecret',//token密钥
     tokenExpiresTime: 60*1
   }
